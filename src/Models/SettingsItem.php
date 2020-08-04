8534b6decea2360568f8e1389182bc6de9422f10
Serhii Kondrashov <garphild28@gmail.com>
2020-08-04 23:29:49 +0300

    Add methods for SettingsItem (change parameters)

--- src/Models/SettingsItem.php
+++ src/Models/SettingsItem.php
@@ -3,6 +3,11 @@
 namespace Garphild\SettingsManager\Models;
 
 class SettingsItem {
+  public const TYPE_BOOLEAN = "boolean";
+  public const TYPE_STRING = "string";
+  public const TYPE_NUMERIC = "numeric";
+  public const TYPE_VARIANTS = "variants";
+
   public $default = '0';
   public $dataType = "boolean";
   public $module = "";
@@ -21,5 +26,45 @@ class SettingsItem {
     if ($item) {
       foreach($item as $n=>$v) $this->{$n} = $v;
     }
+  }
+
+  function setTypeBoolean() {
+    $this->dataType = self::TYPE_BOOLEAN;
+    return $this;
+  }
+
+  function setTypeString() {
+    $this->dataType = self::TYPE_STRING;
+    return $this;
+  }
+
+  function setTypeNumber() {
+    $this->dataType = self::TYPE_NUMERIC;
+    return $this;
+  }
+
+  function setTypeVariants() {
+    $this->dataType = self::TYPE_VARIANTS;
+    return $this;
+  }
+
+  function addVariant($variant) {
+    $this->values[] = $variant;
+    return $this;
+  }
+
+  function disableApiReflect() {
+    $this->showToApi = false;
+    return $this;
+  }
+
+  function setSettingsGroup($group) {
+    $this->settingsGroup = $group;
+    return $this;
+  }
+
+  public function setDefaultValue($value) {
+    $this->default = $value;
+    return $this;
   }
 }
